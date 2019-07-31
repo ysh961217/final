d8878c7ce01bd68bdc36423d22c582cc76944739
- <ysh961217@naver.com>
2019-07-31 14:50:18 +0000

    heroku commit

--- final/settings.py
+++ final/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'fvdim+-@2q(x3c+5ar5um4qla@3znz
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
 
-ALLOWED_HOSTS = [".goorm.io"]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
