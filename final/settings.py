247c950be2babafa38f123597e15a670c8b272fb
- <ysh961217@naver.com>
2019-07-31 14:30:04 +0000

    first commit

--- final/settings.py
+++ final/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'fvdim+-@2q(x3c+5ar5um4qla@3znz4p%6$jkun6oe6hvmzo9h'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'fvdim+-@2q(x3c+5ar5um4qla@3znz4p%6$jkun6oe6hvmzo9h')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
 
 ALLOWED_HOSTS = [".goorm.io"]
 
